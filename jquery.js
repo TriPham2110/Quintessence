37b7925d09979aeb8717d1be6b2d2034c3e1f2f2
Tri Pham <44308446+TriPham2110@users.noreply.github.com>
2019-06-20 00:53:30 -0500

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,9 +1,15 @@
-//var backgroundImg = ['background0.jpg','background1.jpg','background2.jpg','background3.jpg'];
-//$('body').css({'background-image': 'url(../static/' + backgroundImg[Math.floor(Math.random() * backgroundImg.length)] + ')'});
-
 var total = 4;
 function rndBackground(){
 	var num = Math.floor(Math.random() * total);
 	document.body.background = '../static/background'+ num +'.jpg';
 	document.body.style.backgroundRepeat = "repeat";
+}
+
+function checkpassword() {
+	password = document.getElementById("password").value;
+	confirmPassword = document.getElementById("confirmPassword").value;
+	console.log(password);
+	console.log(confirmPassword);
+	if (password == confirmPassword) return true;
+	else return false;
 }
