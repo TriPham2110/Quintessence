84c9231ba546d5fbcbfb358fe2b5d66fb4f1c26d
TriPham2110 <TriPham2110@users.noreply.github.com>
2019-07-07 17:48:15 -0500

    Fixed some looks and functionality of profile page

--- app_needs_serviceAccountKey/static/js/jquery.js
+++ app_needs_serviceAccountKey/static/js/jquery.js
@@ -20,3 +20,20 @@ function showPassword() {
 	else x.type = "password";
 }
 
+function uploadMsg(){
+
+	var input = document.getElementById('file');
+
+	for (var i=0; i<input.files.length; i++){
+		var ext= input.files[i].name.substring(input.files[i].name.lastIndexOf('.')+1).toLowerCase();
+
+		if ((ext == 'jpg') || (ext == 'png')){
+			$("#msg").text("Files are supported")
+		}
+		else{
+			$("#msg").text("Files are NOT supported")
+			document.getElementById("file").value ="";
+		}
+	}
+}
+
